a801dec7ab7084e227d981ecaafc9032cf7615d3
Harsha <ramchandrasriharsha@gmail.com>
2025-12-20 19:51:33 +0530

    annnaaww

---
+++ payment-service/src/main/java/com/payment_service/payment_service/config/ExecutorConfig.java
@@ -0,0 +1,17 @@
+package com.payment_service.payment_service.config;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExecutorConfig {
+
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newFixedThreadPool(10);
+    }
+    
+}
